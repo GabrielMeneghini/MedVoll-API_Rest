6bc4adb64aca71fba8533b316256136486bbc884
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-03-12 23:04:13 -0300

    Criado MedicoRepository

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -18,4 +18,15 @@ public class Endereco {
     private String uf;
     private String numero;
     private String complemento;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+    }
 }
+
