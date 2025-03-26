bc410630aab681e500891abca9f86be0c7a7000f
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-03-26 10:35:27 -0300

    DB V5 e exclusão lógica de médicos

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -18,6 +18,8 @@ public class Medico {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private boolean ativo;
+
     private String nome;
 
     private String email;
@@ -33,6 +35,7 @@ public class Medico {
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -45,5 +48,9 @@ public class Medico {
         if(dados.nome() != null) this.nome = dados.nome();
         if(dados.telefone() != null) this.telefone = dados.telefone();
         if(dados.endereco() != null) this.endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
