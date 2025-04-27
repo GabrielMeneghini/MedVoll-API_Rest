586de945cb7b897583edc6b1c51a81936b4488c6
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-04-27 09:49:25 -0300

    Criada entidade Usuario e DB V8

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,5 @@
 package med.voll.api.controller;
 
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
