69ab953c3a60d8aac2fc59122948602fe9930a27
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-05-20 18:33:58 -0300

    DB V9 e entidade, controller, repository e DTOs de Consulta criados

---
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,32 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "consultas")
+@Getter
+@AllArgsConstructor
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "paciente_id", nullable = false)
+    private Paciente paciente;
+
+    @ManyToOne
+    @JoinColumn(name = "madico_id", nullable = false)
+    private Medico medico;
+
+    @Column(name = "data_hora", nullable = false)
+    private LocalDateTime dataHora;
+
+}
---
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,11 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(@NotNull Long idPaciente,
+                                       Long idMedico,
+                                       @NotNull @Future LocalDateTime dataHora) {
+}
